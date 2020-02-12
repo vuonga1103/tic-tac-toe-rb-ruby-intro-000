b94a0c6a3689065756490603c718a0eb792fe076
Anh Vuong <vuonga1103@yahoo.com>
2020-02-12 22:27:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,7 +45,7 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.chomp()
+  input = gets.chomp().strip()
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index, current_player(board))
